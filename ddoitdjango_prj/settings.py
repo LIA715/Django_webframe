e6ae31702dabb9782e51bc840a03bc31495a7d44
lia715 <sy0715p@naver.com>
2023-05-22 17:43:59 +0900

    23.05.22

--- ddoitdjango_prj/settings.py
+++ ddoitdjango_prj/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog",
     "single_pages",
+    "crispy_forms",
+    "crispy_bootstrap4",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialacoount",
+    "allauth.socialaccount.providers.google",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +127,19 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+#이메일 인증과정은 그냥 넘어가겠다.
+#LOGIN_REDIRECT_URL = "/blog/"
+
